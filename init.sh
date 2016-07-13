4809bae96adbaf360a33f13cca3af10283d50284
Martin Gabelmann <martin@gabelmann.biz>
2016-07-13 21:33:34 +0200

    fix logic

--- init.sh
+++ init.sh
@@ -4,11 +4,12 @@ if [ "$1" == "bash" ]; then
     exec $@
 fi
 
-if [ "$LDAP_SSL" == "true" ] && [ ! "$(ls -A /config/ssl)" ]; then
-   echo "No SSL certificates found,"
-   echo -e "dont forget to run:\nchown 76:70 private.pem\nchmod 'og=-rwx' private.pem"
-   exit 1
-else
+if [ "$LDAP_SSL" == "true" ]; then
+    if [ ! "$(ls -A /config/ssl)" ]; then
+        echo "No SSL certificates found,"
+        echo -e "dont forget to run:\nchown 76:70 private.pem\nchmod 'og=-rwx' private.pem"
+        exit 1
+    fi
     echo "copy public keys to /etc/pki/trust/anchors/ and update trusted certificates"
     cp /config/ssl/*public* /etc/pki/trust/anchors/
     /usr/sbin/update-ca-certificates -f -v
